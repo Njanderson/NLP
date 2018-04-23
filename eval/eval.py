258845eca54b6c43529b58c20097da229fedc6df
Nick Anderson <nja4@uw.edu>
2018-04-22 23:36:47 -0700

    added to eval import path

--- eval/eval.py
+++ eval/eval.py
@@ -1,8 +1,9 @@
 import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 from argparse import ArgumentParser
 from utils import loader
 from models import ngram, cnn
-import io
 from tempfile import TemporaryFile
 
 TEST_DATA_DIR = os.path.join('resources', 'test')
