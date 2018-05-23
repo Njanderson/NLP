295e9dbaeba44b632b39ac45cfa0f92195f00b56
Sijie Chen <nishizhen424@qq.com>
2018-05-23 12:35:08 -0700

    rnn.py in-progress

--- models/rnn.py
+++ models/rnn.py
@@ -1,8 +1,19 @@
 from models.model import Model
-
+import numpy as np
 
 class Rnn(Model):
 
+    def __init__(self, word_dim, hidden_dim=100, bptt_truncate=4):
+        # assign instance variables
+        self.word_dim = word_dim
+        self.hidden_dim = hidden_dim
+        self.bptt_truncate = bptt_truncate
+        
+        # randomly initialize the network parameters
+        self.U = np.random.uniform(-np.sqrt(1./word_dim), np.sqrt(1./word_dim), (hidden_dim, word_dim))
+        self.V = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (word_dim, hidden_dim))
+        self.W = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (hidden_dim, hidden_dim))
+        
     """Train the model. Accepts data, a list of sentences to train on"""
     def train(self, language, data):
         raise NotImplementedError()
